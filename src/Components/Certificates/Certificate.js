d5c32e58cfef2bb00aba17ea2f122380a85a9b58
NikhilMishra <nikhilkumarmishra2@gmail.com>
2022-07-06 07:47:28 +0530

    certificate container fixed

--- src/Components/Certificates/Certificate.js
+++ src/Components/Certificates/Certificate.js
@@ -1,19 +1,9 @@
-import {
-  Box,
-  HStack,
-  Image,
-  Link,
-  Text,
-  VStack,
-  Badge,
-  Button,
-  Icon,
-} from "native-base";
+import { Box, HStack, Image, Link, Text, VStack, Button } from "native-base";
 import React from "react";
 
 function Certificate() {
   return (
-    <Box padding={6} shadow={8} marginTop={6}>
+    <Box padding={6} shadow={8} marginTop={6} maxWidth={"100%"}>
       <Text marginBottom={4} fontWeight={800} fontSize={"xl"}>
         Certifications
       </Text>
@@ -23,6 +13,7 @@ function Certificate() {
         paddingRight={8}
         justifyContent={"space-between"}
         flexDir={["column", "row"]}
+        width={"100%"}
       >
         <HStack marginBottom={2} marginTop={2}>
           <Image
@@ -32,7 +23,7 @@ function Certificate() {
               uri: "https://cdn-icons-png.flaticon.com/512/732/732221.png",
             }}
           />
-          <VStack>
+          <VStack width={"90%"}>
             <Text
               width={"container"}
               fontWeight={550}
@@ -85,7 +76,7 @@ function Certificate() {
               uri: "https://cdn-icons-png.flaticon.com/512/732/732221.png",
             }}
           />
-          <VStack>
+          <VStack width={"90%"}>
             <Text
               width={"container"}
               fontWeight={550}
